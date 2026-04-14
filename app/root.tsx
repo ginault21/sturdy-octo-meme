2e1b5b29da190b757cd90885f62af3926172f512
DeMion Claiborne <demionclaiborne@gmail.com>
2026-04-13 21:26:41 -0400

    Add App Bridge script and API key meta tag to fix custom elements

--- app/root.tsx
+++ app/root.tsx
@@ -6,8 +6,10 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="shopify-api-key" content={process.env.SHOPIFY_API_KEY} />
         <link rel="preconnect" href="https://cdn.shopify.com/" />
         <link rel="stylesheet" href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css" />
+        <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
         <Meta />
         <Links />
       </head>
